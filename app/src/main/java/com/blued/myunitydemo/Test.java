3f55fb638df70331250a1dbe7a91c1a51c4d4434
张宇亮 <zhangyuliang@blued.com>
2020-12-16 11:37:01 +0800

    描述：proto在主工程和组件中切换

---
+++ app/src/main/java/com/blued/myunitydemo/Test.java
@@ -0,0 +1,20 @@
+package com.blued.myunitydemo;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.bumptech.glide.Glide;
+
+/**
+ * @Author： ZhangYuLiang
+ * @description：
+ */
+public class Test {
+
+    private void testLib(){
+
+
+//        ARouter.getInstance().destroy();
+
+//        StudentProto.Student;
+
+    }
+}
--- b_module_one/src/main/java/com/blued/b_module_one/OneActivity.java
+++ b_module_one/src/main/java/com/blued/b_module_one/OneActivity.java
@@ -14,6 +14,7 @@ import com.alibaba.android.arouter.launcher.ARouter;
 import com.blued.c_module_main.MainActivity;
 import com.blued.common.base.BaseActivity;
 import com.blued.common.constants.ARouterConfig;
+import com.blued.myunitydemo.PeopleProto;
 
 /**
  * @Author： ZhangYuLiang
@@ -37,6 +38,8 @@ public class OneActivity extends BaseActivity {
         module2 = findViewById(R.id.bt_module2);
         module3 = findViewById(R.id.bt_module3);
         moduleMain = findViewById(R.id.bt_module_main);
+
+//        PeopleProto.People
     }
 
     private void initListener() {
